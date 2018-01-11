0e81963c0c5f4a40a7f8b51f4d9c42744bc92c0c
surnan <forsuresh@yandex.com>
2018-01-11 15:47:51 -0500

    trying to figure out why final button isn't showing.  Appears to be setup correctly

--- project three/project three/ViewController.swift
+++ project three/project three/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     var myButton = UIButton()
     var myLabel = UILabel()
-    
     var tempMessage = "Blah, Blah, Blah"
     var tempMessage2: String = "" {
         willSet(newScore) {
@@ -21,6 +20,7 @@ class ViewController: UIViewController {
         }
     }
     
+    var finalButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,37 +29,43 @@ class ViewController: UIViewController {
         view.addSubview(myButton)
         myButton.setTitle("Hello World", for: .normal)
         myButton.backgroundColor = UIColor.black
-        
         myButton.addTarget(self, action: #selector(buttonPressed(sender:))  , for: .touchDown)
-        
         myButton.translatesAutoresizingMaskIntoConstraints = false
         myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         myButton.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor, constant: 30).isActive = true
         
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        
         
         myLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(myLabel)
         myLabel.text = tempMessage
         myLabel.textColor = .white
         myLabel.backgroundColor = .blue
-        
         myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         myLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    
+    
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        finalButton.setTitle("FINAL BUTTON", for: .normal)
+        finalButton.setTitleColor(.white, for: .normal)
+        finalButton.backgroundColor = UIColor.blue
+        view.addSubview(finalButton)
+        finalButton.translatesAutoresizingMaskIntoConstraints = false
+        finalButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        finalButton.bottomAnchor.constraint(equalTo: view.readableContentGuide.bottomAnchor, constant: 50).isActive = true
     }
     
     
     
     @objc func buttonPressed(sender: UIButton){
         print("Button Press Happened")
-        
         var field: UITextField?
-        
         let myAlert = UIAlertController(title: "Insert Title", message: "Insert Message", preferredStyle: .alert)
         myAlert.addTextField(configurationHandler: nil)
-
         myAlert.addTextField(){(input: UITextField) in
             input.placeholder = "There's no value"
             input.clearButtonMode = UITextFieldViewMode.whileEditing  //very cool feature
@@ -69,13 +75,10 @@ class ViewController: UIViewController {
         }
         
         func yesHandler(actionTarget: UIAlertAction){
-            print("YES -> !!");
-            print(field!.text!);
-            
+            print("YES -> !!");print(field!.text!);
             print("FIELD INSIDE ===> \(field!.text!)")  //<--- this works
             self.tempMessage2 = field!.text!            //<--- this works
         }
-
         myAlert.addAction(UIAlertAction(title: "SHOW ", style: .default, handler: yesHandler))
         myAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         myAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
@@ -83,4 +86,33 @@ class ViewController: UIViewController {
         show(myAlert, sender: self)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
