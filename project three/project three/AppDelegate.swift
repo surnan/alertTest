a0952c434ba8bbb62fa0ef74ddd4c15e488337f7
surnan <forsuresh@yandex.com>
2018-01-11 13:08:51 -0500

    .addAction multiple times with different styles & sometimes with handler implementations. .addTextField multiple times .placeholder on "addTextField" clear button mode

---
+++ project three/project three/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  colorsSlidersTextFieldsLinked
+//
+//  Created by admin on 1/8/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        //below needed after deleting storyboard
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        //above needed after deleting storyboard
+        
+        return true
+    }
+}
+
+
---
+++ project three/project three/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  project three
+//
+//  Created by admin on 1/11/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myButton = UIButton()
+    var myLabel = UILabel()
+    
+    var tempMessage = "Blah, Blah, Blah"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+        view.addSubview(myButton)
+        myButton.setTitle("Hello World", for: .normal)
+        myButton.backgroundColor = UIColor.black
+        
+        myButton.addTarget(self, action: #selector(buttonPressed(sender:))  , for: .touchDown)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myButton.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor, constant: 30).isActive = true
+        
+        ////
+        
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myLabel)
+        myLabel.text = tempMessage
+        myLabel.textColor = .white
+        myLabel.backgroundColor = .blue
+        
+        myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    
+    
+    @objc func buttonPressed(sender: UIButton){
+        print("Button Press Happened")
+        
+        var field: UITextField?
+        
+        let myAlert = UIAlertController(title: "Insert Title", message: "Insert Message", preferredStyle: .alert)
+        myAlert.addTextField(configurationHandler: nil)
+
+        myAlert.addTextField(){(input: UITextField) in
+            input.placeholder = "There's no value"
+            input.clearButtonMode = UITextFieldViewMode.whileEditing  //very cool feature
+            field = input
+        }
+        
+        
+        func yesHandler(actionTarget: UIAlertAction){
+            print("YES -> !!");
+            print(field!.text!);
+        }
+
+        myAlert.addAction(UIAlertAction(title: "SHOW ", style: .default, handler: yesHandler))
+        myAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        myAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        myAlert.addAction(UIAlertAction(title: "Destructive", style: .destructive, handler: nil))
+        show(myAlert, sender: self)
+    }
+}
+
